ada3f60194b6b8e47cc8cd9d462bc5cc35b4806f
talhakosen <talhakosen@hotmail.com>
2016-06-09 16:15:39 +0300

    find view and add button listener

--- app/src/main/java/tkosen/com/android_realm/MainActivity.java
+++ app/src/main/java/tkosen/com/android_realm/MainActivity.java
@@ -1,18 +1,21 @@
 package tkosen.com.android_realm;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edt_name,edt_population;
+    Button btn_save;
     Realm realm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,23 +24,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         realm = Realm.getDefaultInstance();
 
+        edt_name = (EditText)findViewById(R.id.edt_name);
+        edt_population = (EditText)findViewById(R.id.edt_population);
+        btn_save = (Button) findViewById(R.id.btn_save);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                realm.beginTransaction();
-
-                Country country = realm.createObject(Country.class);
-                country.setCode("90");
-                country.setName("Turkey");
-                country.setPopulation(80000000);
-
-                realm.commitTransaction();
+            public void onClick(View v) {
+                
             }
         });
-
 
     }
 
@@ -57,4 +54,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
